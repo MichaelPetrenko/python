42dbeadc3b7229a58daea692e5c03447e8c3e9f3
severa3 <reizmp4@gmail.com>
2025-02-15 23:49:08 +0300

    synergy tasks 1 and 2

---
+++ helloworld/task2.py
@@ -0,0 +1,12 @@
+# Задание №2
+# Dryopithecus, Australopithecus, Ramapithecus, Homo habilis, Homo erectus, Homo neanderthalensis, and Homo sapiens
+
+stage1 = input("1 этап развития человека: ")
+stage2 = input("2 этап развития человека: ")
+stage3 = input("3 этап развития человека: ")
+stage4 = input("4 этап развития человека: ")
+stage5 = input("5 этап развития человека: ")
+stage6 = input("6 этап развития человека: ")
+stage7 = input("7 этап развития человека: ")
+
+print(stage1, stage2, stage3, stage4, stage5, stage6, stage7, sep = " => ")
