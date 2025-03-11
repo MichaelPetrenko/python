f3f8d493ad3f24b9945d478d81a5d8520d894c82
severa3 <reizmp4@gmail.com>
2025-03-11 10:01:34 +0300

    generate forests, rivers

--- game/map.py
+++ game/map.py
@@ -1,3 +1,5 @@
+from utils import randcell, randbool, randcell2
+
 # 0 - поле
 # 1 - дерево
 # 2 - река
@@ -16,9 +18,24 @@ class Map:
         self.width = width
         self.cells = [[0 for i in range(self.width)] for j in range(self.height)]
 
-    # def generate_rivers(self):
+    def generate_river(self, l):
+        rc = randcell(self.width, self.height)
+        rx, ry = rc[0], rc[1]
+        self.cells[rx][ry] = 2
+        while l > 0:
+            rc2 = randcell2(rx, ry)
+            rx2, ry2 = rc2[0], rc2[1]
+            if self.check_bounds(rx2, ry2):
+                self.cells[rx2][ry2] = 2
+                rx, ry = rx2, ry2
+                l -= 1
 
-    # def generate_forest(self):
+
+    def generate_forest(self, r, mxr): # 3, 10 = 30%
+        for ri in range(self.height):
+            for ci in range(self.width):
+                if randbool(r, mxr):
+                    self.cells[ri][ci] = 1
 
     def check_bounds(self, x, y):
         if (x < 0) or (y < 0) or (x >= self.height) or (y >= self.width):
@@ -35,3 +52,10 @@ class Map:
                     print(CELL_TYPES[cell], end='')
             print("⬛")
         print("⬛" * (self.width + 2))
+
+map = Map(20, 10)
+map.generate_forest(3, 10)
+map.generate_river(10)
+map.generate_river(10)
+map.generate_river(10)
+map.print_map()
