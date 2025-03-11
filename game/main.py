3194459f10154360082c98561da62f0165c5bbc2
severa3 <reizmp4@gmail.com>
2025-03-11 17:39:14 +0300

    game 3 iteration

--- game/main.py
+++ game/main.py
@@ -4,9 +4,13 @@ from map import Map
 import time
 import os
 
-TICK_SLEEP = 0.5
+TICK_SLEEP = 0.05
+TREE_UPDATE = 50
+FIRE_UPDATE = 100
+MAP_WIDTH = 20
+MAP_HEIGHT = 10
 
-map = Map(20, 10)
+map = Map(MAP_WIDTH, MAP_HEIGHT)
 map.generate_forest(3, 10)
 map.generate_river(10)
 map.generate_river(10)
@@ -20,3 +24,7 @@ while True:
     map.print_map()
     tick += 1
     time.sleep(TICK_SLEEP)
+    if tick % TREE_UPDATE == 0:
+        map.generate_tree()
+    if tick % FIRE_UPDATE == 0:
+        map.update_fires()
--- game/map.py
+++ game/map.py
@@ -5,18 +5,22 @@ from utils import randcell, randbool, randcell2
 # 2 - река
 # 3 - госпиталь
 # 4 - апгрейд-шоп
+# 5 - пожар
 
-CELL_TYPES = "⬜🌲🌊🏥🏦"
+CELL_TYPES = "🟩🌲🌊🏥🏦🔥"
 
 class Map:
+
     width = 0
     height = 0
     cells = [[]]
+
 
     def __init__(self, width, height):
         self.height = height
         self.width = width
         self.cells = [[0 for i in range(self.width)] for j in range(self.height)]
+
 
     def generate_river(self, l):
         rc = randcell(self.width, self.height)
@@ -37,11 +41,38 @@ class Map:
                 if randbool(r, mxr):
                     self.cells[ri][ci] = 1
 
+
+    def generate_tree(self):
+        c = randcell(self.width, self.height)
+        cx, cy = c[0], c[1]
+        if self.cells[cx][cy] == 0:
+            self.cells[cx][cy] = 1
+
+
+    def add_fire(self):
+        c = randcell(self.width, self.height)
+        cx, cy = c[0], c[1]
+        if self.cells[cx][cy] == 1:
+            self.cells[cx][cy] = 5
+
+
+    def update_fires(self):
+        for ri in range(self.height):
+            for ci in range(self.width):
+                cell = self.cells[ri][ci]
+                if cell == 5:
+                    self.cells[ri][ci] = 0
+
+        for i in range(5):
+            self.add_fire()
+
+
     def check_bounds(self, x, y):
         if (x < 0) or (y < 0) or (x >= self.height) or (y >= self.width):
             return False
         else:
             return True
+
 
     def print_map(self):
         print("⬛" * (self.width + 2))
