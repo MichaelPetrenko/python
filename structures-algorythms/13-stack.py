0edf50867e8be96a34d5f92cefd436863d406200
severa3 <reizmp4@gmail.com>
2025-08-31 00:39:43 +0300

    sort

---
+++ structures-algorythms/13-stack.py
@@ -0,0 +1,13 @@
+def function1():
+    print("Inside function1")
+    function2()
+
+def function2():
+    print("Inside function2")
+    function3()
+
+def function3():
+    print("Inside function3")
+
+# Вызов функции
+function1()
